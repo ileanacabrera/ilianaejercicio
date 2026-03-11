bee1244e94200d537b4fdaa225ebdd3166ef8a0a
ileanacabrera <ileanavanesaalvarez@gmail.com>
2026-03-10 20:56:35 -0600

    listo

---
+++ mi-app/src/velocidad.js
@@ -0,0 +1,6 @@
+let distancia = parseFloat(prompt("Ingrese la distancia recorrida:"));
+let tiempo = parseFloat(prompt("Ingrese el tiempo empleado:"));
+
+let velocidad = distancia / tiempo;
+
+console.log("La velocidad promedio es: " + velocidad);
